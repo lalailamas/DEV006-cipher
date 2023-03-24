3027240fe9988878145998d3d66453df71bb611e
lalailamas <ale.cordoba.p@gmail.com>
2023-03-24 13:43:04 -0300

    proyecto terminado

--- src/cipher.js
+++ src/cipher.js
@@ -5,57 +5,57 @@
 const cipher = {
   encode: (displacement, messageInitial) => {
     const displacementValue = displacement;
-    const messageValue= messageInitial;
-    let codedMessage= "";
+    const messageValue = messageInitial;
+    let codedMessage = "";
 
     //bucle
-    for (let i= 0; i < messageValue.length; i++) {
+    for (let i = 0; i < messageValue.length; i++) {
       //charCodeAt devuelve el numero indicando el valor del caracter
-      const asciiNumber = messageValue[i].charCodeAt(); 
+      const asciiNumber = messageValue[i].charCodeAt();
       //console.log(asciiNumber);
       //condicional para numeros entre 65 y 90 (letras en mayuscula)
-      if (asciiNumber >= 65 && asciiNumber <=90) {
-        const asciiTextMay= (((asciiNumber - 65 + parseInt(displacementValue))%26)+65);
+      if (asciiNumber >= 65 && asciiNumber <= 90) {
+        const asciiTextMay = (((asciiNumber - 65 + parseInt(displacementValue)) % 26) + 65);
         //fromCharCode devuelve la cadena creada 
         codedMessage += String.fromCharCode(asciiTextMay);
-      //condicional para numeros entre 97 y 122 (letras en minúscula)
-      } else if (asciiNumber >= 97 && asciiNumber <=122) { 
-        const asciiTextMin= (((asciiNumber - 97 + parseInt(displacementValue))%26)+97);
-        codedMessage += String.fromCharCode (asciiTextMin);
-      // condicional para los espacios. Numero 32 corresponde al "espacio" en ascii
+        //condicional para numeros entre 97 y 122 (letras en minúscula)
+      } else if (asciiNumber >= 97 && asciiNumber <= 122) {
+        const asciiTextMin = (((asciiNumber - 97 + parseInt(displacementValue)) % 26) + 97);
+        codedMessage += String.fromCharCode(asciiTextMin);
+        // condicional para los espacios. Numero 32 corresponde al "espacio" en ascii
       } else if (asciiNumber === 32) {
         codedMessage += " ";
-      } else { 
-        codedMessage += string [i];
+      } else {
+        codedMessage += String[i];
       }
-      console.log(codedMessage);
-    } //cierre for
-    return codedMessage; 
+      //console.log(codedMessage);
+    }
+    return codedMessage;
   },
 
   decode: (displacement, messageInitial) => {
     const displacementValue = displacement;
-    const messageValue= messageInitial;
-    let decodedMessage= "";
+    const messageValue = messageInitial;
+    let decodedMessage = "";
 
-    for (let i= 0; i < messageValue.length; i++) {
-      const asciiNumber = messageValue[i].charCodeAt(); 
-  
-      if (asciiNumber >= 65 && asciiNumber <=90) {
-        const asciiTextMay= (((asciiNumber - 65 - parseInt(displacementValue))%26)+65);
+    for (let i = 0; i < messageValue.length; i++) {
+      const asciiNumber = messageValue[i].charCodeAt();
+
+      if (asciiNumber >= 65 && asciiNumber <= 90) {
+        const asciiTextMay = (((asciiNumber - 65 - parseInt(displacementValue)) % 26) + 65);
         decodedMessage += String.fromCharCode(asciiTextMay);
-      } else if (asciiNumber >= 97 && asciiNumber <=122) { 
-        const asciiTextMin= (((asciiNumber - 97 - parseInt(displacementValue))%26)+97);
-        decodedMessage += String.fromCharCode (asciiTextMin);
+      } else if (asciiNumber >= 97 && asciiNumber <= 122) {
+        const asciiTextMin = (((asciiNumber - 97 - parseInt(displacementValue)) % 26) + 97);
+        decodedMessage += String.fromCharCode(asciiTextMin);
       } else if (asciiNumber === 32) {
         decodedMessage += " ";
-      } else { 
-        decodedMessage += string [i];
+      } else {
+        decodedMessage += String[i];
       }
-        console.log (decodedMessage);
+      //console.log(decodedMessage);
 
     }
-    return decodedMessage; 
+    return decodedMessage;
   }
 }
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import cipher from './cipher.js';
 //aca se escuchan eventos del DOM, invocar cipher.encode ()
 
-const displacement = document.getElementById ("displacement");
-const messageInitial = document.getElementById ("messageInitial"); 
-const encodeButton = document.getElementById ("encodeButton");
-const decodedButton = document.getElementById ("decodeButton");
+const displacement = document.getElementById("displacement");
+const messageInitial = document.getElementById("messageInitial");
+const encodeButton = document.getElementById("encodeButton");
+const decodedButton = document.getElementById("decodeButton");
 
-encodeButton.addEventListener ("click", function() {
-  const codedMessage = cipher.encode (displacement.value, messageInitial.value);
+encodeButton.addEventListener("click", function () {
+  const codedMessage = cipher.encode(displacement.value, messageInitial.value);
   messageInitial.value = codedMessage;
 });
 
-decodedButton.addEventListener ("click", function() {
-  const decodedMessage = cipher.decode (displacement.value, messageInitial.value);
+decodedButton.addEventListener("click", function () {
+  const decodedMessage = cipher.decode(displacement.value, messageInitial.value);
   messageInitial.value = decodedMessage;
+});
+
+const sendButton = document.getElementById("sendButton");
+sendButton.addEventListener("click", function () {
+  alert("Tu mensaje ha sido enviado, te contactaremos en breve");
 });
